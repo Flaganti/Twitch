c9b7c958f6081ed60075bbac0844e709c4a0ac8c
Andrej Knez <andrej.531.knez@gmail.com>
2017-11-19 17:56:50 +0100

    Made socket timeout after 5 seconds of not receiving anything.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ try:
     s[0].send("CAP REQ :twitch.tv/commands\r\n".encode("utf-8"))
     #s[0].send("CAP REQ :twitch.tv/tags\r\n".encode("utf-8")) #Needs re changing! so it can read tags
     s[0].send("JOIN {}\r\n".format(config.CHAN).encode("utf-8"))
+    s[0].settimeout(5)
     connected = True #Socket successfully connected
 except Exception as e:
     print(str(e))
@@ -26,25 +27,27 @@ except Exception as e:
 
 def bot_loop(): #TODO: Change to a queueing system so spamm gets proccessed more quickly -> Sending will be restricted to MODRATE or RATE
     while connected:
-        #TODO: Add timer commands (messages that get executed on a set interval)
-        #TODO: Giveaway system
-        response = s[0].recv(1024).rstrip()
-        if response == "PING :tmi.twitch.tv":
-            print(response+"\r\n")
-            s[0].send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-            print("Pong")
-        else:
-            try:
+        try:
+            #TODO: Add timer commands (messages that get executed on a set interval)
+            #TODO: Giveaway system
+            response = s[0].recv(1024).rstrip()
+            if response == "PING :tmi.twitch.tv":
                 print(response+"\r\n")
-                username = re.search(r"\w+", response).group(0)
-                message = CHAT_MSG.sub("", response)
-                print(username + ": " + message+"\r\n")
-                if message.startswith("!",0,1):
-                    utility.func_command(s[0],username,message)
-            except Exception as e:
-                print(e)
-
-        utility.check_timers(s[0]) # Checks if any timers need execution
+                s[0].send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+                print("Pong")
+            else:
+                try:
+                    print(response+"\r\n")
+                    username = re.search(r"\w+", response).group(0)
+                    message = CHAT_MSG.sub("", response)
+                    print(username + ": " + message+"\r\n")
+                    if message.startswith("!",0,1):
+                        utility.func_command(s[0],username,message)
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            utility.check_timers(s[0]) # Checks if any timers need execution
+            #print(e)
         time.sleep(1 / config.MODRATE)
 
 if __name__ == "__main__":
