586caead8715090530f95b11f35a4b94616728c8
A. Knez <Flaganti@users.noreply.github.com>
2017-12-01 12:16:27 +0100

    fixed modrate

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ NICK = "sprout_bot"            # Twitch username your using for your bot
 PASS = "oauth:za9r078fwx8zwd7zc7q9mfeisl0srx" # your Twitch OAuth token
 CHAN = "#flaganti"                   # the channel you want the bot to join.
 RATE = (20/30) # 20 messages in 30 seconds
-MODRATE = (30/30) # 100 messages in 30 seconds
+MODRATE = (100/30) # 100 messages in 30 seconds
 BAN_PAT=[]
