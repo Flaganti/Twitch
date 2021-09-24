eb6017e1f791383599257d8c997e674a85b6c708
Andrej Knez <andrej.knez@flawless-code.com>
2021-09-24 12:33:06 +0200

    Fixing the bot, disabling the oauth token (removing it)

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 HOST = "irc.twitch.tv"              # This is Twitchs IRC server
 PORT = 6667                         # Twitchs IRC server listens on port 6667
-NICK = "flagantibot"            # Twitch username your using for your bot
-PASS = "oauth:ym9imhp7nn6v5f7f12lm4u0l2ry2m4" # your Twitch OAuth token
-CHAN = "#flaganti"                   # the channel you want the bot to join.
+NICK = "twitch_user_name"            # Twitch username your using for your bot
+PASS = "oauth: 123456" # your Twitch OAuth token
+CHAN = "#twitch_channel"                   # the channel you want the bot to join.
 RATE = (20.0/30.0) # 20 messages in 30 seconds
 MODRATE = (100.0/30.0) # 100 messages in 30 seconds
 POINTAMOUNT = 1 #How many points you get
