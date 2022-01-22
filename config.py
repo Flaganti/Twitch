636cdb15d97d919806f1eb319beda1e8efaa35e0
Andrej Knez <andrej.knez@flawless-code.com>
2022-01-22 16:56:07 +0100

    config.py and .gitignore update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 HOST = "irc.twitch.tv"  # This is Twitchs IRC server
 PORT = 6667  # Twitchs IRC server listens on port 6667
-NICK = "#"  # Twitch username your using for your bot
-PASS = "oauth:"  # your Twitch OAuth token
-CHAN = "#"  # the channel you want the bot to join.
+NICK = "#<name of bot>"  # Twitch username your using for your bot
+PASS = "oauth:<oauth token of bot>"  # your Twitch OAuth token -> https://twitchapps.com/tmi/
+CHAN = "#flaganti"  # the channel you want the bot to join.
 RATE = (20.0 / 30.0)  # 20 messages in 30 seconds
 MODRATE = (100.0 / 30.0)  # 100 messages in 30 seconds
 POINTAMOUNT = 1  # How many points you get
@@ -10,3 +10,5 @@ TIMERFORPOINTS = 60 * 60  # How often you get the points in seconds
 VIEWERAPI = "https://tmi.twitch.tv/group/user/{}/chatters".format(CHAN[1:])
 FOLLOW_REQ = False
 BAN_PAT = [ ]
+APP_ACCESS_TOKEN = "" # Register the bot as an application -> clientID
+CLIENT_SECRET = "" # Register the bot as an application -> ClientSecret
